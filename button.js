1dcc1d6f9266d1ce58618a41d3b172582fe5e8b8
Evan Ritscher <eritscher@2u.com>
2018-07-17 20:18:53 -0400

    adds helper for intToHex

--- button.js
+++ button.js
@@ -1,19 +1,6 @@
 const EventEmitter = require('events');
 const pcap = require('pcap');
-
-
-function convertIntToHex(intArray) {
-  const hexArray = [];
-
-  for (let i in intArray) {
-    let currentAsHex = intArray[i].toString(16);
-    if (currentAsHex.length < 2) {
-      currentAsHex = currentAsHex.padStart(1, '0');
-    }
-    hexArray.push(currentAsHex);
-  }
-  return hexArray.join(':')
-}
+const convertIntToHex = require('./helpers');
 
 
 class Button extends EventEmitter {
---
+++ helpers.js
@@ -0,0 +1,12 @@
+module.exports = function convertIntToHex(intArray) {
+  const hexArray = [];
+
+  for (let i in intArray) {
+    let currentAsHex = intArray[i].toString(16);
+    if (currentAsHex.length < 2) {
+      currentAsHex = currentAsHex.padStart(1, '0');
+    }
+    hexArray.push(currentAsHex);
+  }
+  return hexArray.join(':')
+}
