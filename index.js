9822a5e0395e2ac5697af73e9d4e1ef120f6469c
Evan Ritscher <eritscher@2u.com>
2018-07-19 20:59:44 -0400

    makes index more package-importable

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 const Dash = require('./button');
-const buttonConfig = require('./buttonConfig');
-const buttons = new Dash(buttonConfig);
-
-buttons.on('pressed', function (evt) {
-    console.log('The button has been pressed.');
-    console.log(evt);
-});
+module.exports = Dash;
