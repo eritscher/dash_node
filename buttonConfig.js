3183b5b22f94c26dca1b47ebe0a2228156729202
Evan Ritscher <eritscher@2u.com>
2018-07-26 20:13:59 -0400

    minor format changes

---
+++ buttonConfig.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        address: '78:e1:03:cf:ae:1c',
+        name: 'Funyuns'
+    },
+    {
+        address: '18:74:2e:d0:fd:d8',
+        name: 'init'
+    },
+    {
+        address: '78:e1:03:48:29:a9',
+        name: 'Cafe Bustelo'
+    }
+];
--- helpers.js
+++ helpers.js
@@ -1,13 +1,13 @@
 module.exports = function convertIntToHex(intArray) {
-  const hexArray = [];
+    const hexArray = [];
 
-  for (let i in intArray) {
-    let currentAsHex = intArray[i].toString(16);
-    if (currentAsHex.length < 2) {
-      currentAsHex = currentAsHex.padStart(2, '0');
+    for (let i in intArray) {
+        let currentAsHex = intArray[i].toString(16);
+        if (currentAsHex.length < 2) {
+            currentAsHex = currentAsHex.padStart(2, '0');
+        }
+        hexArray.push(currentAsHex);
     }
-    hexArray.push(currentAsHex);
-  }
 
-  return hexArray.join(':')
-}
+    return hexArray.join(':');
+};
